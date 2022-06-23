662cc4b027bf4efea13ddc5ee41b1e6be45a7107
yasmim <yasmim.25022003@gmail.com>
2022-06-23 17:21:17 -0300

    estrutura atual do form de contato

---
+++ validation.js
@@ -0,0 +1,15 @@
+function validar(){
+    //cria variavel local recebendo valor do form user usando name´s dos inputs//
+
+    var nome = formUser.nome.value;
+    var email = formUser.email.value;
+    var telefone = formUser.telefone.value;
+    var endereco = formUser.endereco.value;
+    var senha = formUser.senha.value;
+
+    if(nome =="" || nome.length <3){
+        alert("Informe o nome completo!");
+        formUser.nome.focus();
+        return false;
+    }
+}

b664825993e9362639a2cc67966167f2ecb56654
yasmim <yasmim.25022003@gmail.com>
2022-06-28 17:03:35 -0300

    formulario

--- validation.js
+++ validation.js
@@ -5,6 +5,8 @@ function validar() {
     var email = formUser.email.value;
     var telefone = formUser.telefone.value;
     var endereco = formUser.endereco.value;
+    var cpf = formUser.cpf.value;
+    var cnpj = formUser.cnpj.value;
     var senha = formUser.senha.value;
 
     if (nome == "" || nome.length < 3) {
@@ -12,4 +14,55 @@ function validar() {
         formUser.nome.focus();
         return false;
     }
+
+    if (email == "" || email.length < 10) {
+        alert("E-mail não atende os requisitos!");
+        formUser.email.focus();
+        return false;
+    }
+
+    if (telefone == "" || telefone.length < 11) {
+        alert("Telefone não atende os requisitos!");
+        formUser.telefone.focus();
+        return false;
+    }
+
+    if (endereco == "" || endereco.length < 5) {
+        alert("Endereço não atende os requisitos!");
+        formUser.endereco.focus();
+        return false;
+    }
+
+    if (cnpj == "") {
+        if (cpf == "" || cpf.length < 14 && cnpj != "") {
+            alert("CPF inválido!");
+            formUser.cpf.focus();
+            return false;
+        }
+    } else if (cnpj == "" || cnpj < 18) {
+        alert("CNPJ inválido!");
+        formUser.cnpj.focus();
+        return false;
+    } 
+
+    if (cpf == "") {
+        if (cnpj == "" || cnpj.length < 18 && cpf != "") {
+            alert("CNPJ inválido!");
+            formUser.cnpj.focus();
+            return false;
+        }
+    } else if (cpf == "" || cpf < 14) {
+        alert("CPF inválido!");
+        formUser.cpf.focus();
+        return false;
+    } 
+
+
+    if (senha == "" || senha.length <= 6) {
+        alert("Senha deve ter no mínimo 6 digitos!");
+        formUser.senha.focus();
+        return false;
+    }
+
+
 }
